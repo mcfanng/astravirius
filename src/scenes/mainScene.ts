05ba7f44f07d8b8ad5b6b42b5594a8b1239a9bca
Gabe <mcfanng@live.com>
2019-11-29 13:57:48 -0500

    GM: add cat

--- src/scenes/mainScene.ts
+++ src/scenes/mainScene.ts
@@ -1,9 +1,11 @@
 
 import 'phaser';
-import * as space1 from '../../assets/images/Space1.png';
+import * as cat from '../../assets/sprites/cat.png';
+import * as red from '../../assets/sprites/red.png';
+import * as space1 from '../../assets/sprites/Space1.png';
 export class MainScene extends Phaser.Scene {
 
-  private phaserSprite: Phaser.GameObjects.Sprite;
+  // private phaserSprite: Phaser.GameObjects.Sprite;
 
   constructor() {
       super({
@@ -13,11 +15,26 @@ export class MainScene extends Phaser.Scene {
 
   public preload(): void {
       this.load.image('background', space1);
-      console.log(this.plugins);
+      this.load.image('cat', cat);
+      this.load.image('red', red);
     }
 
   public create(): void {
-      this.phaserSprite = this.add.sprite(400, 300, 'background');
-      console.log(this.phaserSprite);
+      this.add.sprite(400, 300, 'background');
+      const particles = this.add.particles('red');
+
+      const emitter = particles.createEmitter({
+          speed: 100,
+          scale: { start: 1, end: 0 },
+          blendMode: 'ADD'
+      });
+
+      const logo = this.physics.add.image(400, 100, 'cat');
+
+      logo.setVelocity(100, 200);
+      logo.setBounce(1, 1);
+      logo.setCollideWorldBounds(true);
+
+      emitter.startFollow(logo);
     }
   }
